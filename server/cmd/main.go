3ab15006c1c1d84c38357f7ccca97473cea34526
thesyscoder <pruthvirajs2007@outlook.com>
2025-07-16 12:00:00 +0530

    server: logger fixed and add .env

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/thesyscoder/kylon/internal/infrastructure/config"
 	"github.com/thesyscoder/kylon/pkg/logger"
 )
 
@@ -9,4 +13,14 @@ func main() {
 	logger.SetLogger("debug")
 	log := logger.GetLogger()
 	log.Info("[Main]: Kylon Backend Server: Starting up...")
+
+	// Load the configuration
+	var err error
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		// Log to stderr directly before logger is fully configured
+		fmt.Fprintf(os.Stderr, "Fatal: Failed to load configuration: %v\n", err)
+		os.Exit(1)
+	}
+	log.Debug(cfg)
 }
